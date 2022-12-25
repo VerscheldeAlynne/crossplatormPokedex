53078dbdb1b87296c2978f3723adae5665379de5
Alynne Verschelde <alynne.verschelde@gmail.com>
2022-12-25 22:11:54 +0100

    extra errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Pokedex } from './Pokedex';
-import styles from "./pokedexStylesheet.css";
+import styles from './styles';
 
 
 export default function App() {
@@ -12,42 +12,3 @@ export default function App() {
     </View>
   );
 }
-
-/**const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  pokedex: {
-    backgroundColor: '#e61515',
-    width: 848,
-    margin: 0 ,
-    padding: 1 ,
-    borderRadius: 15,
-    borderColor:  '#000' ,
-    border: 10,
-    flex: 1,
-},
-screen: {
-  borderRadius: 3,
-  FontFace: "VT323",
-  borderColor: '#879a65',
-  border: 3,
-},
-})
-*/
-
-/*
-fetch('Your URL to fetch data from', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    firstParam: 'yourValue',
-    secondParam: 'yourOtherValue',
-  }),
-});*/
